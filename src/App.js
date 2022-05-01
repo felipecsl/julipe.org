f1350cb851c1ce9a8aaf5cd2e239b6227fed106a
Felipe Lima <felipe.lima@gmail.com>
2022-04-30 17:09:32 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="p-10">
+      <header className="header">
+        <h1 className="text-3xl">J & F</h1>
       </header>
+      <section className="menu mt-10">
+        <nav className="menu">
+          <ul>
+            <li>
+              <a
+                href="https://theknot.com/julipe"
+                target="_blank"
+                className="underline"
+                rel="noopener noreferrer"
+              >
+                Wedding website
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://forms.gle/yCgD3hBzZzVk5RMQ9"
+                target="_blank"
+                className="underline"
+                rel="noopener noreferrer"
+              >
+                RSVP here
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
     </div>
   );
 }
